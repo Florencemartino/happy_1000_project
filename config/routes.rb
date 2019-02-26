ac87f6dc9f8bd35ad5d5a5e2af39da57b2ef9d4b
Remi Achji <achji.remi@gmail.com>
2019-02-26 12:23:55 +0100

    saving

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'events#index'
+
+  resources :events do
+    resources :whishlists
+    resources :guests
+  end
+  resources :guests, only: [:create, :update]
+  resources :baskets, only: [:create]
+
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

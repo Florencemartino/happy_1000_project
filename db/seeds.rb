78e23a4fdcdc1f7d105059e5f4b8bf5c08995e33
Susan HA <ha.susan@outlook.fr>
2019-02-26 14:56:43 +0100

    creation items seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Item.destroy_all
+
+
+puts 'Creating 10 vegetables items...'
+10.times do
+  item = Item.create!(
+    name: Faker::Food.vegetables,
+    category: "légumes"
+  )
+end
+puts 'Finished!'
+
+

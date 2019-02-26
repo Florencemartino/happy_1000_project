e35e17ce81fc44530e4ae15885253ac591c5f82d
Remi Achji <achji.remi@gmail.com>
2019-02-26 14:29:10 +0100

    routes valides

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
     resources :baskets
   end
 
-  resources :items
+  # resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

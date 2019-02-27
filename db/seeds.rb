c38876fa933a6f7073334d92409131d5b324d590
samir aissioui <samir.aissioui@sciencespo.fr>
2019-02-27 18:46:38 +0100

    Whishlist : choice of quantity

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,35 +63,20 @@ tomato = Item.create!(name: "tomate", category: "salé", icon: "icons.tomato.png
 vodka = Item.create!(name: "vodka", category: "alcool", icon: "icons/vodka.png")
 wine = Item.create!(name: "wine", category: "alcool", icon: "icons/wine.png")
 
-
-<<<<<<< HEAD
-puts 'Creating 10 vegetables items...'
-30.times do
-  item = Item.create!(
-    name: Faker::Food.vegetables,
-    category: "légumes"
-  )
-end
-
-=======
->>>>>>> master
-
 puts 'Creating whishlist...'
 
-ingredient_1 = Whishlist.create!(item_id: Item.first.id, event_id: event_1.id, quantity: 4)
-ingredient_2 = Whishlist.create!(item_id: Item.second.id, event_id: event_1.id, quantity: 2)
-ingredient_3 = Whishlist.create!(item_id: Item.third.id, event_id: event_1.id, quantity: 5)
-ingredient_4 = Whishlist.create!(item_id: Item.fourth.id, event_id: event_1.id, quantity: 3)
-ingredient_5 = Whishlist.create!(item_id: Item.fifth.id, event_id: event_1.id, quantity: 3)
+ingredient_1 = Whishlist.create!(item_id: Item.first.id, event_id: event_1.id, quantity: 0)
+ingredient_2 = Whishlist.create!(item_id: Item.second.id, event_id: event_1.id, quantity: 0)
+ingredient_3 = Whishlist.create!(item_id: Item.third.id, event_id: event_1.id, quantity: 0)
+ingredient_4 = Whishlist.create!(item_id: Item.fourth.id, event_id: event_1.id, quantity: 0)
+ingredient_5 = Whishlist.create!(item_id: Item.fifth.id, event_id: event_1.id, quantity: 0)
 
 
 puts 'Creating basket...'
 my_basket1 = Basket.create!(whishlist_id: Whishlist.first.id, user_id: remi.id, quantity: 2)
 my_basket2 = Basket.create!(whishlist_id: Whishlist.second.id, user_id: flo.id, quantity: 2)
-<<<<<<< HEAD
-=======
-
->>>>>>> master
+my_basket3 = Basket.create!(whishlist_id: Whishlist.third.id, user_id: sam.id, quantity: 1)
+my_basket4 = Basket.create!(whishlist_id: Whishlist.fourth.id, user_id: susan.id, quantity: 2)
 
 
 puts 'Finished!'

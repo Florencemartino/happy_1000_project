8c9a3db245d36bcdbaf1a78030bc1fde136b1907
Florence Martino <florence.martino.fm@gmail.com>
2019-02-28 17:29:54 +0100

    ingredients

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,8 +1,18 @@
 class BasketsController < ApplicationController
   before_action :authenticate_user!
 
+  def new
+    @event = Event.find(params[:event_id])
+    @basket = Basket.new
+  end
+
   def index
+    @event = Event.find(params[:event_id])
     @whishlists = Whishlist.all
+  end
+
+  def edit
+    raise
   end
 
   def show
@@ -12,5 +22,11 @@ class BasketsController < ApplicationController
 
   def select_items
     @whishlists = Whishlist.all
+  end
+
+  private
+
+  def basket_params
+    params.require(:basket).permit(:price_in_cent)
   end
 end
--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,6 +1,7 @@
 class Basket < ApplicationRecord
   belongs_to :whishlist
   belongs_to :user
+  has_many :items, through: :whishlists
 
   validates :quantity, presence: true
 end

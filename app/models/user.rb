5f71c0ba7d9d021c85e0454e28b842a4100c1fc8
Remi Achji <achji.remi@gmail.com>
2019-03-01 16:00:27 +0100

    saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events, dependent: :destroy
+
   has_many :baskets
   has_many :guests
   has_many :whishlists, through: :baskets

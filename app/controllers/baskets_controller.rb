f55ffaf731843ab96a31914faaa16d4ecafd4685
samir aissioui <samir.aissioui@sciencespo.fr>
2019-03-01 15:54:42 +0100

    update quantity done

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -5,6 +5,7 @@ class BasketsController < ApplicationController
   def index
     @event = Event.find(params[:event_id])
     @whishlists = Whishlist.where(event_id: params[:event_id])
+    # @baskets = current_user.baskets.join(whishlists).where(whishlist: {event: @event})
     all_user_baskets = current_user.baskets
     @baskets = []
     all_user_baskets.each do |b|
@@ -21,7 +22,7 @@ class BasketsController < ApplicationController
     @basket = Basket.new
     @basket.whishlist = @whishlist
     @basket.user = current_user
-    @basket.quantity = 0
+    @basket.quantity = 1
     @basket.price_in_cent = 0
     @basket.save
     redirect_to event_baskets_path(@event)
@@ -30,16 +31,23 @@ class BasketsController < ApplicationController
   def update
     @event = Event.find(params[:event_id])
     @basket = Basket.find(params[:id])
-    @whishlist = Whishlist.find(params[:id])
+    @whishlist = @basket.whishlist
     case params[:sens]
     when "1"
-      then @basket.quantity += 1 && @whishlist.quantity -= 1
+      @basket.quantity += 1
+      @whishlist.quantity -= 1
     when "-1"
-      then @basket.quantity -= 1 && @whishlist.quantity += 1
+      @basket.quantity -= 1
+      @whishlist.quantity += 1
     end
     @whishlist.save!
     @basket.save!
     redirect_to event_baskets_path(@event)
+  end
+
+  def edit
+    @basket = Basket.find(params[:id])
+    @event = Event.find(params[:event_id])
   end
 
   # def show

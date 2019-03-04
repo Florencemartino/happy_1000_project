3c9b828f583934bad81e859b8932f557de22b893
Remi Achji <achji.remi@gmail.com>
2019-03-04 10:52:53 +0100

    saving

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,18 @@
 import "bootstrap";
 import 'plugins/flatpickr'
 
+import { initAutocomplete } from '../plugins/init_autocomplete';
+initAutocomplete();
+
 import { initialization } from '../components/dynamicnavbar';
 import { toggleDesc } from '../components/description';
 
 initialization();
-toggleDesc();
+
+
+
+if (document.querySelector(".fa-caret-square-down")) {
+  toggleDesc();
+}
 
 

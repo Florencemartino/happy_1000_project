474dce3127e8f9572a05d748af12077a3004457a
remi achji <achji.remi@gmail.com>
2019-03-04 22:44:16 +0100

    saving

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,25 @@
 import "bootstrap";
 import 'plugins/flatpickr'
 
+// Slider for the events#index
 import { navigation } from '../components/slider';
 
 if (document.querySelector(".slider.step2")) {
   navigation();
 }
 
-
+// Slider for the whishlist#index
 import { foodNav } from '../components/slider-wishlist';
 
 if (document.querySelector(".slider2.sale")) {
   foodNav();
 }
 
+// Slide navbar
 import { initialization } from '../components/dynamicnavbar';
 initialization();
 
+// Autocomplete for address in event creation
 import { initAutocomplete } from '../plugins/init_autocomplete';
 initAutocomplete();
 
